59cab9cfb7e21151697e156dabc5af894437f66e
Jeremy Nation <jeremy@jeremynation.me>
2016-07-11 19:05:42 -0400

    Make blocks fall, add end condition

--- src/game.js
+++ src/game.js
@@ -3,60 +3,154 @@
  */
 var Game = {};
 
-Game.getEmptyGrid = function(gridWidth, gridHeight) {
-    if ((gridWidth % 2 !== 0) ||
-        (gridHeight % 2 !== 0)) {
-        throw new Error('bad grid dimensions');
-    }
-
-    var grid = [];
-    for (var i = 0; i < gridHeight; i++) {
-        grid.push(Array(gridWidth).fill(Game.emptyColor));
-    }
-    return grid;
-};
-
-Game.getBlocks = function(emptyColor) {
-    return [
-        [
-            [emptyColor, 'red', 'red', emptyColor],
-            [emptyColor, 'red', 'red', emptyColor]
-        ]
-    ];
-};
-
-Game.initialize = function(borderColor, emptyColor, squareDim) {
+Game.initialize = function(borderColor, squareDim) {
     Game.borderColor = borderColor;
-    Game.emptyColor = emptyColor;
     Game.squareDim = squareDim;
 
+    Game.directions = {
+        DOWN: 'down',
+        LEFT: 'left',
+        RIGHT: 'right'
+    };
+
+    Game.gridStates = {
+        EMPTY: 'empty',
+        SQUARE: 'square'
+    };
+
+    Game.colors = {};
+    Game.colors[Game.gridStates.EMPTY] = 'white';
+    Game.colors[Game.gridStates.SQUARE] = 'red';
+
     Game.display = document.getElementById('display');
+
+    var gridWidth = Game.display.width / Game.squareDim;
+    var gridHeight = Game.display.height / Game.squareDim;
+    if ((gridWidth % 2 !== 0) || (gridHeight % 2 !== 0)) {
+        throw new Error('bad grid dimensions');
+    } else {
+        Game.grid = [];
+        for (var rowNum = 0; rowNum < gridHeight; rowNum++) {
+            var row = [];
+            for (var colNum = 0; colNum < gridWidth; colNum++) {
+                row.push({
+                    state: Game.gridStates.EMPTY,
+                    isActive: false
+                });
+            }
+            Game.grid.push(row);
+        }
+    }
+
+    Game.allBlocks = [
+        [
+            [Game.gridStates.SQUARE, Game.gridStates.SQUARE],
+            [Game.gridStates.SQUARE, Game.gridStates.SQUARE]
+        ]
+    ];
+
+    Game.addNewBlock(Game.grid, Game.allBlocks);
+
     Game.ctx = Game.display.getContext('2d');
-    Game.grid = Game.getEmptyGrid(
-        Game.display.width / Game.squareDim,
-        Game.display.height / Game.squareDim
-    );
-    Game.blocks = Game.getBlocks(Game.emptyColor);
+
+    Game.draw(Game.ctx, Game.grid, Game.squareDim);
+
+};
+
+Game.addNewBlock = function(grid, allBlocks) {
+    var funcStatus = true;
+
+    var newBlock = allBlocks[Math.floor(Math.random() * allBlocks.length)];
+    var startColNum = (grid[0].length / 2) - Math.floor((newBlock[0].length / 2));
+    for (var rowNum = 0; rowNum < newBlock.length; rowNum++) {
+        for (var colNum = 0; colNum < newBlock[0].length; colNum++) {
+            if (grid[rowNum][startColNum + colNum]['state'] === Game.gridStates.EMPTY) {
+                grid[rowNum][startColNum + colNum]['state'] = newBlock[0][colNum];
+                grid[rowNum][startColNum + colNum]['isActive'] = true;
+            } else {
+                funcStatus = false;
+                break;
+            }
+        }
+        if (!funcStatus) {
+            break;
+        }
+    }
+
+    return funcStatus;
+};
+
+Game.moveActiveBlock = function(grid, direction) {
+    var funcStatus = true;
+
+    var oldActiveCoords = [];
+    for (var rowNum = 0; rowNum < grid.length; rowNum++) {
+        for (var colNum = 0; colNum < grid[0].length; colNum++) {
+            if (grid[rowNum][colNum]['isActive']) {
+                oldActiveCoords.push([rowNum, colNum])
+            }
+        }
+    }
+
+    var moveFunc;
+    if (direction === Game.directions.DOWN) {
+        moveFunc = function(coord) {
+            return [coord[0] + 1, coord[1]]
+        };
+    }
+    var newActiveCoords = oldActiveCoords.map(moveFunc);
+
+    var moveIsAllowed = true;
+    for (var i = 0; i < newActiveCoords.length; i++) {
+        var newCoord = newActiveCoords[i];
+        moveIsAllowed = (
+            (0 <= newCoord[0]) && (newCoord[0] < grid.length) &&
+            (0 <= newCoord[1]) && (newCoord[1] < grid[0].length) &&
+            (
+                (grid[newCoord[0]][newCoord[1]]['isActive'] === true) ||
+                (grid[newCoord[0]][newCoord[1]]['state'] === Game.gridStates.EMPTY)
+            )
+        );
+        if (!moveIsAllowed) {
+            break;
+        }
+    }
+
+    if (moveIsAllowed) {
+        var state = grid[oldActiveCoords[0][0]][oldActiveCoords[0][1]]['state'];
+        for (var i = 0; i < oldActiveCoords.length; i++) {
+            grid[oldActiveCoords[i][0]][oldActiveCoords[i][1]]['state'] = Game.gridStates.EMPTY;
+            grid[oldActiveCoords[i][0]][oldActiveCoords[i][1]]['isActive'] = false;
+        }
+        for (var i = 0; i < newActiveCoords.length; i++) {
+            grid[newActiveCoords[i][0]][newActiveCoords[i][1]]['state'] = state;
+            grid[newActiveCoords[i][0]][newActiveCoords[i][1]]['isActive'] = true;
+        }
+    }
+
+    funcStatus = moveIsAllowed;
+    return funcStatus;
 };
 
 Game.update = function(grid) {
-    var newBlockStartPosition = (grid[0].length / 2) - 2;
-
-    var newBlock = Game.blocks[
-        Math.floor(Math.random() * (Game.blocks.length))
-        ];
-
-    for (var i = 0; i < newBlock[0].length; i++) {
-        grid[0][newBlockStartPosition + i] = newBlock[0][i];
-        grid[1][newBlockStartPosition + i] = newBlock[1][i];
+    var keepGoing = true;
+    var moveWorked = Game.moveActiveBlock(grid, Game.directions.DOWN);
+    if (!moveWorked) {
+        for (var rowNum = 0; rowNum < grid.length; rowNum++) {
+            for (var colNum = 0; colNum < grid[0].length; colNum++) {
+                grid[rowNum][colNum]['isActive'] = false;
+            }
+        }
+        keepGoing = Game.addNewBlock(grid, Game.allBlocks);
     }
+    return keepGoing;
 };
 
 Game.draw = function(ctx, grid, squareDim) {
     ctx.strokeStyle = Game.borderColor;
     for (var rowNum = 0; rowNum < grid.length; rowNum++) {
         for (var colNum = 0; colNum < grid[0].length; colNum++) {
-            ctx.fillStyle = grid[rowNum][colNum];
+            ctx.fillStyle = Game.colors[grid[rowNum][colNum]['state']];
             ctx.fillRect(
                 colNum * squareDim,
                 rowNum * squareDim,
@@ -74,7 +168,11 @@ Game.draw = function(ctx, grid, squareDim) {
 };
 
 Game.main = function() {
-    Game.update(Game.grid);
+    var keepGoing = Game.update(Game.grid);
     Game.draw(Game.ctx, Game.grid, Game.squareDim);
-    window.requestAnimationFrame(Game.main);
+    if (keepGoing) {
+        window.requestAnimationFrame(Game.main);
+    } else {
+        console.log('game over!');
+    }
 };
