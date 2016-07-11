0ac98707a2d4cb01f93898a8ecff07f3ef36d362
Jeremy Nation <jeremy@jeremynation.me>
2016-07-11 10:39:43 -0400

    Draw 2x2 block in grid

--- src/game.js
+++ src/game.js
@@ -8,6 +8,13 @@ Game.emptyColor = 'white';
 
 Game.squareDim = 50;
 
+Game.blocks = [
+    [
+        [Game.emptyColor, 'red', 'red', Game.emptyColor],
+        [Game.emptyColor, 'red', 'red', Game.emptyColor]
+    ]
+];
+
 Game.getEmptyGrid = function(gridWidth, gridHeight) {
     if ((gridWidth % 2 !== 0) ||
         (gridHeight % 2 !== 0)) {
@@ -25,15 +32,19 @@ Game.drawGrid = function(ctx, grid, squareDim) {
     ctx.strokeStyle = this.borderColor;
     for (var rowNum = 0; rowNum < grid.length; rowNum++) {
         for (var colNum = 0; colNum < grid[0].length; colNum++) {
-            ctx.rect(
+            ctx.fillStyle = grid[rowNum][colNum];
+            ctx.fillRect(
                 colNum * squareDim,
                 rowNum * squareDim,
                 squareDim,
                 squareDim
             );
-            ctx.fillStyle = grid[rowNum][colNum];
-            ctx.fill();
-            ctx.stroke();
+            ctx.strokeRect(
+                colNum * squareDim,
+                rowNum * squareDim,
+                squareDim,
+                squareDim
+            );
         }
     }
 };
@@ -47,6 +58,17 @@ Game.main = function() {
     );
 
     var ctx = display.getContext('2d');
+
+    var newBlockStartPosition = (grid[0].length / 2) - 2;
+
+    var newBlock = Game.blocks[
+        Math.floor(Math.random() * (Game.blocks.length))
+    ];
+
+    for (var i = 0; i < newBlock[0].length; i++) {
+        grid[0][newBlockStartPosition+i] = newBlock[0][i];
+        grid[1][newBlockStartPosition+i] = newBlock[1][i];
+    }
 
     this.drawGrid(ctx, grid, this.squareDim);
 };
